5a0bf5eb65721652d58e5f79c417ac695e27df82
bcv002 <afromero@bcv002.uniandes.edu.co>
2018-08-29 12:08:35 +0000

    Reducing crop size

--- data_loader.py
+++ data_loader.py
@@ -351,7 +351,7 @@ def get_loader(metadata_path, crop_size, image_size, batch_size, \
   # resize = image_size + (image_size//20)
   crop_size = image_size 
 
-  transform_resize = [transforms.Resize((crop_size+10, crop_size+10), interpolation=Image.ANTIALIAS)] if crop_size==64 else [] 
+  transform_resize = [transforms.Resize((crop_size+5, crop_size+5), interpolation=Image.ANTIALIAS)] if crop_size==64 else [] 
 
   if mode == 'train':
     if color_jitter:
