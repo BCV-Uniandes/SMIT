2bfb97075df4c97a9c47bd0574fc9d4852ae9eb0
bcv002 <afromero@bcv002.uniandes.edu.co>
2018-10-25 08:27:32 +0000

    Improving circle/color visualization :art:

--- misc/utils.py
+++ misc/utils.py
@@ -1,5 +1,54 @@
 #=======================================================================================#
 #=======================================================================================#
+def circle_frame(tensor, thick=5, color='green', first=False):
+  import ipdb
+  import numpy as np, torch
+  _color_ = {'green': (-1,1,-1), 'red': (1,-1,-1), 'blue': (-1,-1,1)}
+  _tensor = tensor.clone()
+  size = tensor.size(2)
+  thick=((size/2)**2)/10
+  xx, yy = np.mgrid[:size, :size]
+  circle = (xx - size/2) ** 2 + (yy - size/2) ** 2  
+  donut = np.expand_dims(np.logical_and(circle < ((size/2)**2+ thick), circle > ((size/2)**2 - thick)), 0)*1.
+  donut = donut.repeat(tensor.size(1), axis=0)
+  for i in range(donut.shape[0]):
+    donut[i] = donut[i]*_color_[color][i]
+  # ipdb.set_trace()
+  donut = to_var(torch.FloatTensor(donut), volatile=True)
+  for nn in [0,-1]: #First and last frame
+    _tensor[nn] = tensor[nn] + donut
+
+  return _tensor  
+
+#=======================================================================================#
+#=======================================================================================#
+def color_frame(tensor, thick=5, color='green', first=False):
+  import ipdb
+  _color_ = {'green': (-1,1,-1), 'red': (1,-1,-1), 'blue': (-1,-1,1)}
+  # tensor = to_data(tensor)
+  # ipdb.set_trace()
+  for i in range(thick):
+    for k in range(tensor.size(1)):
+      for nn in [0,-1]: #First and last frame
+        tensor[nn,k,i,:] = _color_[color][k]
+        if first: tensor[nn,k,:,i] = _color_[color][k]
+        tensor[nn,k,tensor.size(2)-i-1,:] = _color_[color][k]
+        tensor[nn,k,:,tensor.size(2)-i-1] = _color_[color][k]
+  return tensor
+
+#=======================================================================================#
+#=======================================================================================#
+def create_circle(size=256):
+  import numpy as np
+  xx, yy = np.mgrid[:size, :size]
+  # circles contains the squared distance to the (size, size) point
+  # we are just using the circle equation learnt at school
+  circle = (xx - size/2) ** 2 + (yy - size/2) ** 2
+  bin_circle = (circle<=(size/2)**2)*1.
+  return bin_circle
+
+#=======================================================================================#
+#=======================================================================================#
 def create_dir(dir):
   import os
   if '.' in os.path.basename(dir):
@@ -24,7 +73,16 @@ def get_aus(image_size, dataset, attr=None):
   resize = lambda x: skimage.transform.resize(x, (image_size, image_size))
   if dataset not in ['EmotionNet', 'BP4D']:
     from data.attr2img import external2img
-    labels = ['Source']+attr.selected_attrs
+    selected_attrs = []
+    for attr in attr.selected_attrs:
+      if attr=='Male': attr = 'Male/_Female'
+      elif attr=='Young': attr = 'Young/_Old'
+      elif 'Hair' in attr: pass
+      elif dataset=='CelebA': attr+='_Swap'
+      else: pass
+      selected_attrs.append(attr)
+
+    labels = ['Source']+selected_attrs
     imgs = external2img(labels, img_size=image_size)
     imgs = [resize(np.array(img)).transpose(2,0,1) for img in imgs]
   else:
@@ -116,6 +174,46 @@ def PRINT(file, str):
 
 #=======================================================================================#
 #=======================================================================================#
+def plot_txt(txt_file):
+  import matplotlib.pyplot as plt
+  lines = [line.strip().split() for line in open(txt_file).readlines()]
+  legends = {idx:line for idx, line in enumerate(lines[0][1:])} #0 is epochs
+  lines = lines[1:]
+  epochs = []
+  losses = {loss:[] for loss in legends.values()}
+  for line in lines:
+    epochs.append(line[0])
+    for idx, loss in enumerate(line[1:]):
+      losses[legends[idx]].append(float(loss))
+
+  # import matplotlib as mpl
+  # mpl.use('Agg')
+  import matplotlib.pyplot as plt
+  import pylab as pyl  
+  plot_file = txt_file.replace('.txt','.pdf')
+  _min = 4 if len(losses.keys())>9 else 3
+  for idx, loss in enumerate(losses.keys()):
+    # plot_file = txt_file.replace('.txt','_{}.jpg'.format(loss))
+
+    plt.rcParams.update({'font.size': 10})    
+    ax1=plt.subplot(3, _min, idx+1)
+    # err = plt.plot(epochs, losses[loss], 'r.-')
+    err = plt.plot(epochs, losses[loss], 'b.-')
+    plt.setp(err, linewidth=2.5)
+    plt.ylabel(loss.capitalize(), fontsize=16)
+    plt.xlabel('Epoch', fontsize=16)
+    ax1.tick_params(labelsize=8)
+    plt.hold(False)
+    # plt.legend(['Training', 'Validation'], loc=1,prop={'size':10})
+    # ax1.text(ax1.get_xlim()[1]*1.08, ax1.get_ylim()[1]*0.88, params, style='italic', size=7,
+    #         bbox={'facecolor':'green', 'alpha':0.5, 'pad':3})
+    plt.grid()
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None,\
+                      wspace=0.5, hspace=0.5)
+  pyl.savefig(plot_file, dpi=100)
+
+#=======================================================================================#
+#=======================================================================================#
 def pdf2png(filename):
   from wand.image import Image
   from wand.color import Color
@@ -130,21 +228,6 @@ def pdf2png(filename):
 def replace_weights(target, source, list):
   for l in list:
     target[l] = source[l] 
-
-#=======================================================================================#
-#=======================================================================================#
-def slerp(val, low, high):
-  """
-  original: Animating Rotation with Quaternion Curves, Ken Shoemake
-  https://arxiv.org/abs/1609.04468
-  Code: https://github.com/soumith/dcgan.torch/issues/14, Tom White
-  """
-  import numpy as np
-  omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
-  so = np.sin(omega)
-  if so == 0:
-    return (1.0-val) * low + val * high # L'Hopital's rule/LERP  
-  return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high  
 
 #=======================================================================================#
 #=======================================================================================#
@@ -165,6 +248,31 @@ def send_mail(body="bcv002", attach=[], subject='Message from bcv002', to='rv.an
 
 #=======================================================================================#
 #=======================================================================================#
+def single_source(tensor):
+  import torch, math
+  source = torch.ones_like(tensor)
+  middle = int(math.ceil(tensor.size(0)/2.))-1
+  source[middle] = tensor[0]
+  return source
+
+
+#=======================================================================================#
+#=======================================================================================#
+def slerp(val, low, high):
+  """
+  original: Animating Rotation with Quaternion Curves, Ken Shoemake
+  https://arxiv.org/abs/1609.04468
+  Code: https://github.com/soumith/dcgan.torch/issues/14, Tom White
+  """
+  import numpy as np
+  omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
+  so = np.sin(omega)
+  if so == 0:
+    return (1.0-val) * low + val * high # L'Hopital's rule/LERP  
+  return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high  
+
+#=======================================================================================#
+#=======================================================================================#
 def target_debug_list(size, dim, config=None):
   import torch, ipdb
   target_c= torch.zeros(size, dim)
@@ -174,7 +282,7 @@ def target_debug_list(size, dim, config=None):
     # if config.dataset_fake in ['RafD', 'painters_14', 'Animals', 'Image2Weather', 'Image2Season'] and j==0: continue
     # if j==0: continue
     if j>0: target_c[:,j-1]=1 
-    if not config.RafD_FRONTAL:
+    if not config.RafD_FRONTAL and not config.RafD_EMOTIONS:
       if config.dataset_fake=='RafD' and j==0: target_c[:,2] = 1
       if config.dataset_fake=='RafD' and j<=5: target_c[:,10] = 1
       if config.dataset_fake=='RafD' and j>=6: target_c[:,2] = 1
