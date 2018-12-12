a9d84a4626c9d66499b525d7345300385a57ae01
bcv002 <afromero@bcv002.uniandes.edu.co>
2018-12-12 12:44:08 +0000

    Style issue corrections

--- datasets/CelebA.py
+++ datasets/CelebA.py
@@ -64,8 +64,10 @@ class CelebA(Dataset):
                     dict_.items(), key=lambda kv: (kv[1], kv[0]),
                     reverse=True):
                 total += value
-                if self.mode == 'train': PRINT(f, '{} {}'.format(key, value))
-            if self.mode == 'train': PRINT(f, 'TOTAL {}'.format(total))
+                if self.mode == 'train': 
+                    PRINT(f, '{} {}'.format(key, value))
+            if self.mode == 'train': 
+                PRINT(f, 'TOTAL {}'.format(total))
 
     def preprocess(self):
         attrs = self.lines[1].split()
@@ -107,24 +109,19 @@ class CelebA(Dataset):
         # if self.shuffling: random.shuffle(lines)
         for i, line in enumerate(lines):
             splits = line.split()
-            if self.splits[splits[0]] not in self.mode_allowed: continue
+            if self.splits[splits[0]] not in self.mode_allowed: 
+                continue
             if self.mode_data == 'faces':
                 filename = os.path.abspath('data/CelebA/Faces/{}'.format(
                     splits[0]))
             else:
                 filename = os.path.abspath('data/CelebA/data_align/{}'.format(
                     splits[0]))
-            if not os.path.isfile(filename): continue
+            if not os.path.isfile(filename): 
+                continue
             values = splits[1:]
 
             label = []
-            # for idx, value in enumerate(values):
-            #   attr = self.idx2attr[idx]
-            #   if attr in self.selected_attrs:
-            #     if value == '1':
-            #       label.append(1)
-            #     else:
-            #       label.append(0)
 
             for attr in self.selected_attrs:
                 selected_value = values[self.all_attr2idx[attr]]
