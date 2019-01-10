9645f6b071cb5e5ff83482a2408940de0a0742f5
bcv002 <afromero@bcv002.uniandes.edu.co>
2019-01-10 14:09:50 +0000

    Bug CelebA download and dataload

--- generate_data/download.sh
+++ generate_data/download.sh
@@ -7,7 +7,8 @@ fi
 
 kaggle datasets download -d jessicali9530/celeba-dataset
 ZIP_FILE=celeba-dataset.zip
-FOLDER=./CelebA
+FOLDER=./data/CelebA
+mkdir -p $FOLDER
 unzip $ZIP_FILE -d $FOLDER
 rm $ZIP_FILE
 
@@ -15,6 +16,6 @@ mv $FOLDER/list_eval_partition.csv $FOLDER/train_val_test.txt #Train/Val/Test Pa
 mv $FOLDER/list_attr_celeba.csv $FOLDER/list_attr_celeba.txt #List Attributes
 
 # CelebA aligned
-unzip $FOLDER/img_align_celeba.zip -d $FOLDER/data_align
+unzip $FOLDER/img_align_celeba.zip -d $FOLDER
 rm $FOLDER/img_align_celeba.zip
 rm $FOLDER/*.csv
